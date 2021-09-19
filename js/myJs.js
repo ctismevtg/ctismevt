ecdc5e677365de4b84b6380999fd2a79cf4eebd7
ctismevtg <91001242+ctismevtg@users.noreply.github.com>
2021-09-19 15:23:14 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -10,7 +10,6 @@ const textConfig = {
   text9: "Vì cậu đẹp try vlllll",
   text10: "Tớ biết mà ^^ Yêu cậu 300.000",
   text11:
-    "Tối nay tớ quaậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
     "Tối nay tớ quaậu đi chơi nhaa :v Còn giờ thì chnữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
